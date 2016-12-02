1a3541b97369ef71520ec29dd4b93a27ed92c9ba
Salina Wu <salinawu@uchicago.edu>
2016-12-01 20:47:09 -0600

    displays url comments

--- firebase/app.js
+++ firebase/app.js
@@ -25,47 +25,76 @@
     });
   };
 
-  // when element is added, create a new list element to display
-  urlRef.on('child_added', snap => {
-    const li = document.createElement('li');
-    li.innerText = JSON.stringify(snap.val(), null, 3);
-    li.id = snap.key;
-
-    const comments = document.createElement('comments')
-    comments.innerText = snap.val()['comments'];
-    li.ul = comments;
-
-    urlList.appendChild(li);
-
+  // add the button and functionality for upvoting a url
+  // receives the list item to attach the button too and the data corresponding to LI to use
+  function upvoting(snap, li) {
     const upvote = document.createElement('upvote');
     upvote.innerHTML = "Upvote";
     li.appendChild(upvote);
     upvote.addEventListener(
       "click", function() {
         const curr_upvotes = snap.val().upvotes;
-        console.log(curr_upvotes);
         urlRef.child(snap.key).update({
           upvotes: curr_upvotes + 1
         });
       }
     )
+  };
+
+  // displaying comments functionality
+  function display_comments(snap, all_comments) {
+    //initialize to empty array and then if there are comments, get comments.
+    var comments_arr = [];
+    if (snap.val()['comments']) {
+      comments_arr = Object.keys(snap.val()['comments']).map(function(k) { return snap.val()['comments'][k] });
+    };
+
+    // make a new element for each comment and attach to broader 'all_comments' umbrella element
+    comments_arr.map(function(k) {
+      const comments = document.createElement('comments');
+      comments.innerText = k;
+      all_comments.appendChild(comments);
+    });
+  };
+
+  // when element is added, create a new list element to display
+  urlRef.on('child_added', snap => {
+    const li = document.createElement('li');
+    li.innerText = snap.key;
+    li.id = snap.key;
+
+    // add upvoting functionality and attach upvotes to display
+    const upvotes = document.createElement('upvotes');
+    upvotes.innerText = snap.val()['upvotes'];
+    li.appendChild(upvotes);
+    upvoting(snap, li);
+
+    // render comments into an umbrella element and attach to the corresponding url
+    const all_comments = document.createElement('all_comments');
+    display_comments(snap, all_comments);
+    li.appendChild(all_comments);
+
+    // attach this added url to list of urls displayed
+    urlList.appendChild(li);
+
   });
 
   // similarly, change the list view when a url is changed
   urlRef.on('child_changed', snap => {
     const liChanged = document.getElementById(snap.key);
-    liChanged.innerText = JSON.stringify(snap.val(), null, 3);
-    const upvote = document.createElement('upvote');
-    upvote.innerHTML = "Upvote";
-    liChanged.appendChild(upvote);
-    upvote.addEventListener(
-      "click", function() {
-        const curr_upvotes = snap.val().upvotes;
-        urlRef.child(snap.key).update({
-          upvotes: curr_upvotes + 1
-        });
-      }
-    )
+    liChanged.innerText = snap.key;
+
+    // upvote display and path to logic
+    const upvotes = document.createElement('upvotes');
+    upvotes.innerText = snap.val()['upvotes'];
+    liChanged.appendChild(upvotes);
+    upvoting(snap, liChanged);
+
+    // comments display
+    const all_comments = document.createElement('all_comments');
+    display_comments(snap, all_comments);
+    liChanged.appendChild(all_comments);
+
   });
 
   // ...and remove url from view when it's removed from database
